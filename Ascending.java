4912249308862046b82d9be9ac06aa5bc02950ad
QaAssociate <39580252+QaAssociate@users.noreply.github.com>
2018-05-26 09:42:20 +0530

    Add files via upload

---
+++ Ascending.java
@@ -0,0 +1,19 @@
+//Write a java program to sort the numbers in ascending and descending order.
+package com.piedistrict;
+
+public class Ascending
+{
+
+	public static void main(String[] args) 
+	{
+		int i;
+		System.out.println("Ascending Order");
+		for (i = 1; i <= 10; i++)
+			System.out.println(i);
+		System.out.println("Descending Order");
+		for (i = 10; i >= 1; --i)
+			System.out.println(i);
+
+	}
+
+}
